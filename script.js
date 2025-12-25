2971b309cdb043748a6442c7bd3a61fab100c0e1
Mikhi Hunter <hunterc2408@gmail.com>
2025-12-25 01:35:38 -0600

    Feat: fresh start

--- script.js
+++
@@ -1,9 +0,0 @@
-const card = document.querySelector('.card');
-
-card.addEventListener('mouseenter', () => {
-    card.classList.add('opened');
-});
-
-card.addEventListener('click', () => {
-    card.classList.toggle('opened');
-});
